c6be59fa31aff928874321924271e21faefeb551
Kayla <kyairlakayla.davis@gmail.com>
2021-08-14 12:06:49 -0400

    generate html file

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const profileDataArgs = process.argv.slice(2);
-const printProfileData = profileDataArr => {
-    // This... 
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console,log(profileDataArr[i]); 
-    }
-    console.log('================');
+const fs = require('fs'); 
+const generatePage = require('./src/page-template.js');
 
-    // Is the same as this... 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-}; 
+const profileDataArgs = process.argv.slice(2); 
 
-printProfileData(profileDataArgs); 
+const [name, github] = profileDataArgs; 
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
