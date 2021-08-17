249bc2dd4606b34d77a48efc59b6139195f92043
Kayla <kyairlakayla.davis@gmail.com>
2021-08-17 18:15:55 -0400

    pushing with known issue

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 const inquirer = require('inquirer'); 
-
-const fs = require('fs'); 
 const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github); 
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -137,8 +128,18 @@ const promptProject = portfolioData => {
 promptUser() 
 .then(promptProject)
 .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //    if (err) throw new Error(err);
-   //});
+    return generatePage(portfolioData);
+})
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+})
+.then(copyFileResponse => {
+    console.log(copyFileResponse);
+})
+.catch(err => {
+    console.log(err);
 });
