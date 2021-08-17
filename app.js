c8d73d4d24b79192639dae910dd34f690922041d
Kayla <kyairlakayla.davis@gmail.com>
2021-08-17 16:44:15 -0400

    finalize template and functionality

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const inquirer = require('inquirer'); 
 
-// const fs = require('fs'); 
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs'); 
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github); 
 
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//    if (err) throw new Error(err);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
   
-//    console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -137,11 +137,8 @@ const promptProject = portfolioData => {
 promptUser() 
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
+    const pageHTML = generatePage(portfolioData);
     // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    //    if (err) throw new Error(err);
+   //});
 });
